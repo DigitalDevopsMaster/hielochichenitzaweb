b8f94f0a0a1503f6e2f0930f3b7777daddb506d0
Isaac Sauri <isaacsauri@digitaldevops.com.mx>
2024-03-14 00:35:37 -0600

    contenido agregado

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
-//import { initScrollIntegration, initV1 } from "http://lib.digitaldevops.com.mx/v1.js";
-import { initScrollIntegration, initV1 } from "../vendor/v1.js";
+import { initScrollIntegration, initV1 } from "http://lib.digitaldevops.com.mx/v1.js";
+// import { initScrollIntegration, initV1 } from "../vendor/v1.js";
 
 initV1({
   style: 'webLayout',
   contactInfo: {
     companyName: "Hielo Chichén Itzá",
     email: "hielochichenitza@email.com",
-    phone: "999 999999",
+    phone: "(999) 1148769",
     logo: "./assets/logo.png"
   },
   palette: {
     textColor: '#ffffff',                   // Blanco para texto principal
     textSecondaryColor: '#292929',          // Blanco desaturado para texto secundario
+    menuButtonText: '#292929',              // Blanco desaturado para texto secundario
 
     primaryColor: '#6699cc',                // Azul hielo (para bordes y elementos suaves)
     secondaryColor: '#ffffff',              // Blanco (para elementos adicionales)
@@ -29,6 +30,7 @@ initV1({
     subtitleTextColor: '#b3b3b3',           // Blanco desaturado para texto de subtítulos
 
     breakpoint: 600,
+    maxWidth: 1300
   },
   topBarConfig: {
     transparentOnHomeScrollZero: true
